e679c03b11be2f7c1f676e1ee536912a10950058
NoDocCat <nodoccat@outlook.com>
2017-09-17 14:31:14 +0800

    读完parseHTML方法

--- src/core/parseHTML.js
+++ src/core/parseHTML.js
@@ -1,65 +1,59 @@
-define([
-    "../core",
-    "../var/document",
-    "./var/rsingleTag",
-    "../manipulation/buildFragment",
-
-    // This is the only module that needs core/support
-    "./support"
+/**
+ * 将HTML字符串解析为一个包含DOM元素的数组
+ */
+define(["../core", "../var/document", "./var/rsingleTag", "../manipulation/buildFragment", "./support"
 ], function (jQuery, document, rsingleTag, buildFragment, support) {
-
     "use strict";
 
-// Argument "data" should be string of html
-// context (optional): If specified, the fragment will be created in this context,
-// defaults to document
-// keepScripts (optional): If true, will include scripts passed in the html string
+    /**
+     * 将HTML字符串解析为一个DOM数组
+     * @param   data        {String}    用于解析的HTML字符串
+     * @param   context     {Element}   DOM元素上下文
+     * @param   keepScripts {Boolean}   是否保留脚本
+     */
     jQuery.parseHTML = function (data, context, keepScripts) {
+        //当不是HTML字符串时返回空数组
         if (typeof data !== "string") {
             return [];
         }
+        //利用typeof进行方法重载
         if (typeof context === "boolean") {
             keepScripts = context;
             context = false;
         }
 
-        var base, parsed, scripts;
-
+        //当context未指定时,初始化上下文
         if (!context) {
-
-            // Stop scripts or inline event handlers from being executed immediately
-            // by using document.implementation
+            //当浏览器支持时,创建一个新的document实现
             if (support.createHTMLDocument) {
                 context = document.implementation.createHTMLDocument("");
-
-                // Set the base href for the created document
-                // so any parsed elements with URLs
-                // are based on the document's URL (gh-2965)
-                base = context.createElement("base");
+                var base = context.createElement("base");
                 base.href = document.location.href;
                 context.head.appendChild(base);
             } else {
-                context = document;
+                context = document;         //使用当前的document作为上下文
             }
         }
 
-        parsed = rsingleTag.exec(data);
-        scripts = !keepScripts && [];
+        var parsed = rsingleTag.exec(data);     //单标记测试
+        var scripts = !keepScripts && [];
 
-        // Single tag
+        //当HTML字符串为单标记时,直接使用createElement创建元素
         if (parsed) {
             return [context.createElement(parsed[1])];
         }
 
+        //调用buildFragment,创建HTML片段
         parsed = buildFragment([data], context, scripts);
 
+        //移除script
         if (scripts && scripts.length) {
             jQuery(scripts).remove();
         }
 
+        //返回合并后的结果
         return jQuery.merge([], parsed.childNodes);
     };
 
     return jQuery.parseHTML;
-
 });
